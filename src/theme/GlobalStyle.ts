27cdfcf71785bd17cf7c57aa0b990e7bc2c3f6e6
Damian Kubecki <damian_094@wp.pl>
2022-08-03 15:54:18 +0200

    Update font family

--- src/theme/GlobalStyle.ts
+++ src/theme/GlobalStyle.ts
@@ -5,19 +5,19 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+    font-family: 'Caveat', cursive;
+    font-family: 'Rajdhani', sans-serif;
   }
   
   html {
     font-size: 62.5%; 
+
   }
   
   body {
     margin: 0;
     padding: 0;
     font-size: 1.6rem;
-    font-family: "Montserrat", sans-serif;
   }
   `;
 
