4ed8c4a5911828d9f73d7f286822ce1b43a5def6
Damian Kubecki <damian_094@wp.pl>
2022-08-05 16:05:20 +0200

    Update theme

--- src/theme/GlobalStyle.ts
+++ src/theme/GlobalStyle.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import { theme } from './theme';
 
 const GlobalStyle = createGlobalStyle`  
   *, *::before, *::after {
@@ -6,18 +7,19 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     font-family: 'Caveat', cursive;
-    font-family: 'Rajdhani', sans-serif;
+    font-family: 'Roboto', sans-serif;
   }
   
   html {
     font-size: 62.5%; 
-
   }
   
   body {
     margin: 0;
     padding: 0;
     font-size: 1.6rem;
+    min-height: 100vh;
+    background-color: ${theme.primary};
   }
   `;
 
