e31495ed34f2f78def8b4407847dd84694aefed5
Damian Kubecki <damian_094@wp.pl>
2022-08-08 18:31:38 +0200

    Update HeaderLinks component

--- src/components/HeaderLinks/HeaderLinks.tsx
+++ src/components/HeaderLinks/HeaderLinks.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
 import useFontAwesome from 'hooks/useFontAwesome';
-import { githubLink, facebookLink, linkedinLink } from 'config';
 import { IContactLink } from 'types/types';
 import { Wrapper, Item } from './HeaderLinksElements';
 
-const HeaderLinks = () => {
-  const { Icon } = useFontAwesome();
+interface HeaderLinksProps {
+  linksList: IContactLink[];
+}
 
-  const itemsCollection: IContactLink[] = [
-    {
-      link: githubLink,
-      icon: ['fab', 'github'],
-    },
-    {
-      link: facebookLink,
-      icon: ['fab', 'facebook'],
-    },
-    {
-      link: linkedinLink,
-      icon: ['fab', 'linkedin-in'],
-    },
-  ];
+const HeaderLinks = ({ linksList }: HeaderLinksProps) => {
+  const { Icon } = useFontAwesome();
 
   return (
     <Wrapper>
-      {itemsCollection.map(item => (
+      {linksList.map(item => (
         <Item href={item.link} key={item.link} target="_blank">
           <Icon color="#ababab" size="3x" icon={item.icon} />
         </Item>
