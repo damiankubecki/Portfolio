58d6fb4c19a86de2a707f9f52e8a86f771928aaf
Damian Kubecki <damian_094@wp.pl>
2022-08-08 18:37:02 +0200

    Update HeaderTitle component - style changes

--- src/components/HeaderTitle/HeaderTitleElements.ts
+++ src/components/HeaderTitle/HeaderTitleElements.ts
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 const Title = styled.h1`
   color: ${({ theme }) => theme.fontColor};
   font-size: ${({ theme }) => theme.fontSize.xxxl};
-  font-weight: ${({ theme }) => theme.boldFont};
+  font-weight: ${({ theme }) => theme.semiFont};
 `;
 
 const Subtitle = styled.h2`
   margin: 5px 5px;
   color: ${({ theme }) => theme.fontColor};
   font-size: ${({ theme }) => theme.fontSize.xxl};
-  font-weight: ${({ theme }) => theme.lightFont};
+  font-weight: ${({ theme }) => theme.semiFont};
 `;
 
 export { Title, Subtitle };
