e5997bcee101b7145fbec6f5ee30b1e0ef326d1c
Damian Kubecki <damian_094@wp.pl>
2022-08-10 14:36:14 +0200

    Update HeaderArrowDown component

--- src/components/HeaderArrowDown/HeaderArrowDown.tsx
+++ src/components/HeaderArrowDown/HeaderArrowDown.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import useFontAwesome from 'hooks/useFontAwesome';
 import { useTheme } from 'styled-components';
+import { arrowDownIcon } from 'config';
 import { ITheme } from 'types/types';
 import { Wrapper } from './HeaderArrowDownElements';
 
@@ -14,7 +15,7 @@ const HeaderArrowDown = ({ handleClick }: HeaderArrowDownProps) => {
 
   return (
     <Wrapper onClick={handleClick}>
-      <Icon color={theme.additional} size="3x" icon={['fas', 'angles-down']} />
+      <Icon color={theme.additional} size="3x" icon={arrowDownIcon} />
     </Wrapper>
   );
 };
