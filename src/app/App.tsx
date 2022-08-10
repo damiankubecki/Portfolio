7151aeed20627d7939b012f50aafb5af9fe8b01b
Damian Kubecki <damian_094@wp.pl>
2022-08-10 13:24:17 +0200

    Update App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import AppTemplate from 'templates/AppTemplate';
-import { Wrapper, Title, Description } from './AppElements';
+import { Wrapper, Header, Main, Footer } from './AppElements';
 
 const App = () => (
   <AppTemplate>
     <Wrapper>
-      <Title>Create React App TS</Title>
-      <Description>The site is ready to work with TypeScript!</Description>
+      <Header />
+      <Main />
+      <Footer />
     </Wrapper>
   </AppTemplate>
 );
